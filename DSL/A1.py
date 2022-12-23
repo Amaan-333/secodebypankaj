bab46bcd54b4f47e87ed596edccdf0ca3d936314
p13ankaj <98219223+p13ankaj@users.noreply.github.com>
2022-12-23 13:19:56 +0530

    Update A1.py

--- DSL/A1.py
+++ DSL/A1.py
@@ -4,23 +4,23 @@ def getmarks():
     global object
     object=0
     n=int(input("Enter the number of students:"))
-    print("Enter a for absent student")
+    print("Enter -1 for absent student")
     for i in range(n):
         print("Enter the marks of student",i+1)
-        m=input()
-        if m.isdigit():
+        m=int(input())
+        if m!=-1:
             marks.append(m)
         else:
             object+=1
 
     print("The marks are:",marks)
 
-def averagemarks():
-    global avg
+def avgmarks():
+    sum=0
     avg=0
     for i in marks:
-        avg=avg+int(i)
-    avg=avg/(len(marks)-object)
+        sum=sum+i
+    avg=sum/len(marks)
     print("The average marks are:",avg)
 
 def high():
@@ -37,15 +37,36 @@ def low():
             low=i
     print("The lowest marks are:",low)
 
+def highfreq():
+    max=0
+    freq=0
+    point =0
+    for i in marks:
+        freq=marks.count(i)
+        if freq>max:
+            max=freq
+            point =i
+    print("The marks with highest freq is ",point," of times ",max)
+
 def main():
     getmarks()
-    ch=int(input("Enter your choice 1.avgerage marks 2.highest marks 3. lowest marks"))
-    if ch==1:
-        avgmarks()
-    elif ch==2:
-        high()
-    elif ch==3:
-        low()
-    else:
-        break
+    while True:
+        print("1. Average marks")
+        print("2. Highest marks")
+        print("3. Lowest marks")
+        print("4. Highest frequency")
+        print("5. Exit")
+        ch=int(input("Enter your choice:"))
+        if ch==1:
+            avgmarks()
+        elif ch==2:
+            high()
+        elif ch==3:
+            low()
+        elif ch==4:
+            highfreq()
+        elif ch==5:
+            break
+        else:
+            print("Invalid choice")
 main()
